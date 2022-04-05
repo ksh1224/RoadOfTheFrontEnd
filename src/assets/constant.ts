62db9a09082d3a548a5ab2aa1630cf2071ed7888
권수현 <3a9lyd1224@gmail.com>
2022-04-05 17:13:49 +0900

    기본 asset 세팅

---
+++ src/assets/constant.ts
@@ -0,0 +1,40 @@
+import {Dimensions, Platform} from 'react-native';
+
+export const white = '#FFFFFF';
+export const black = '#000000';
+
+// Grayscale Colors
+export const gray1 = '#222222';
+export const gray2 = '#333333';
+export const gray3 = '#4F4F4F';
+export const gray4 = '#828282';
+export const gray5 = '#BDBDBD';
+export const gray6 = '#E0E0E0';
+export const gray7 = '#F2F2F2';
+export const gray8 = '#F9F9F9';
+
+// Primary Colors
+export const red = '#F24147';
+export const blue = '#007AFF';
+export const yellow = '#F8CF3E';
+
+export const kakaoColor = '#FFDE00';
+export const naverColor = '#58B93E';
+
+export const notoSansKrBlack = 'NotoSansKR-Black';
+export const notoSansKrBold = 'NotoSansKR-Bold';
+export const notoSansKrMedium = 'NotoSansKR-Medium';
+export const notoSansKrRegular = 'NotoSansKR-Regular';
+export const notoSansKrThin = 'NotoSansKR-Thin';
+export const notoSansKrLight = 'NotoSansKR-Light';
+
+export const deviceHeight = Dimensions.get('window').height;
+export const deviceWidth = Dimensions.get('window').width;
+export const perDeviceWidth = deviceWidth / 100;
+export const perDeviceHeight = deviceHeight / 100;
+export const isAndroid = Platform.OS === 'android';
+
+export enum ScreenId {
+  APP = 'APP',
+  HOME = 'HOME',
+}
---
+++ src/assets/svgs/weather/index.ts
@@ -0,0 +1,14 @@
+export {default as CelsiusIcon} from './celsius.svg';
+export {default as CloudIcon} from './cloud.svg';
+export {default as CloudyIcon} from './cloudy.svg';
+export {default as DustIcon} from './dust.svg';
+export {default as DegreesIcon} from './degrees.svg';
+export {default as FogIcon} from './fog.svg';
+export {default as HailIcon} from './hail.svg';
+export {default as HotIcon} from './hot.svg';
+export {default as HorizonIcon} from './horizon.svg';
+export {default as LightningIcon} from './lightning.svg';
+export {default as RainIcon} from './rain.svg';
+export {default as SnowIcon} from './snow.svg';
+export {default as SunnyIcon} from './sunny.svg';
+export {default as WindyIcon} from './windy.svg';
