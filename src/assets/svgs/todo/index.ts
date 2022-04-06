932d7fedcb638f10b79ec30c49479b8f4599f5ea
권수현 <3a9lyd1224@gmail.com>
2022-04-06 12:55:14 +0900

    에셋 수정 및 추가

---
+++ src/assets/svgs/todo/index.ts
@@ -0,0 +1,3 @@
+export {default as ToDoIcon} from './todo.svg';
+export {default as CheckedCheckBoxIcon} from './checkedCheckbox.svg';
+export {default as UncheckedCheckBoxIcon} from './uncheckedCheckbox.svg';
