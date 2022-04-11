cfd88dcfac0f1d331d4ab24e011b220665d7de84
권수현 <3a9lyd1224@gmail.com>
2022-04-11 12:17:39 +0900

    최초 1회 권한 요청(임시)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {RootNavigation} from 'components/navigation';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {permissionRequestAll} from 'utils/permissionUtil';
 
 const App = () => {
+  useEffect(() => {
+    // 필요한 모든 권한 요청
+    permissionRequestAll();
+  }, []);
   return (
     <SafeAreaProvider>
       <NavigationContainer>
